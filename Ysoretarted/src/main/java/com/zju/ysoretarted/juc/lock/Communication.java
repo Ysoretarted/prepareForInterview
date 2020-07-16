74219c7b642912799362dec12510aaa97039d8de
1216676256 <835379639@qq.com>
2020-07-16 13:42:22 +0800

    一些lock()、notify、wait、await、park、interrupt拾遗

---
+++ Ysoretarted/src/main/java/com/zju/ysoretarted/juc/lock/Communication.java
@@ -0,0 +1,207 @@
+package com.zju.ysoretarted.juc.lock;
+
+/**
+ * @author zhongcz
+ * @CreateTime 2020/7/16 8:54
+ */
+
+import org.omg.PortableServer.THREAD_POLICY_ID;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.LockSupport;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 线程之间的通信
+ */
+public class Communication {
+
+
+    public static void testSleep() throws InterruptedException {
+        synchronized (Communication.class) {
+            System.out.println("sleep前");
+            System.out.println(Thread.holdsLock(Communication.class));
+            Thread.sleep(1000);
+            System.out.println("sleep后");
+        }
+        System.out.println(Thread.holdsLock(Communication.class));
+    }
+
+    public static void testJoin() {
+        Thread thread1 = new Thread(() -> {
+            System.out.println("我开始了");
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("我结束了");
+        });
+
+        Thread thread2 = new Thread(() -> {
+            System.out.println("线程二开始了");
+        });
+
+        thread1.start();
+        try {
+            thread1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread2.start();
+    }
+
+    public static void testYield() {
+        Thread thread1 = new Thread(() -> {
+            synchronized (Communication.class) {
+                System.out.println("我开始了");
+                Thread.yield();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("我结束了");
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            synchronized (Communication.class) {
+                System.out.println("线程二开始了");
+            }
+        });
+
+        thread1.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread2.start();
+    }
+
+
+    public static void testWaitAndNotify() {
+        String str = "zhongchangze";
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < 10; ++i) {
+            threads[i] = new Thread(() -> {
+                synchronized (str) {
+                    System.out.println(Thread.currentThread().getName() + "我开始了");
+                    try {
+                        str.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + "我被唤醒了 我结束了");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }, "wait 线程" + i);
+            threads[i].start();
+        }
+
+        Thread thread2 = new Thread(() -> {
+            synchronized (str) {
+                System.out.println("我成功抢到锁了");
+                str.notify();
+                System.out.println("我 唤醒其他 线程");
+            }
+        }, "唤醒线程");
+        thread2.start();
+    }
+
+    public static void testLockAndUnlock() {
+        ReentrantLock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
+        //Condition condition1 = lock.newCondition();
+
+        Thread thread2 = new Thread(() -> {
+            lock.lock();
+            try {
+                System.out.println(Thread.currentThread().getName() + "线程2 抢到锁啦");
+            } finally {
+                condition.signalAll();
+                System.out.println(Thread.currentThread().getName() + "我要释放锁了");
+                lock.unlock();
+            }
+
+        });
+        for (int i = 0; i < 10; ++i) {
+            Thread thread1 = new Thread(() -> {
+                lock.lock();
+                try {
+                    System.out.println(Thread.currentThread().getName() + "我上锁了AA" + "然后执行await");
+                    condition.await();
+                    System.out.println(Thread.currentThread().getName() + "我重新获得锁了");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    System.out.println(Thread.currentThread().getName() + "我要释放锁了");
+                    lock.unlock();
+                }
+            }, "await 线程" + i);
+            thread1.start();
+
+            if (i == 5)
+                thread2.start();
+        }
+
+        //thread1.start();
+        //thread2.start();
+    }
+
+    public static void testParkAndUnpark() throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            System.out.println("随便做点什么");
+            LockSupport.park();
+            System.out.println("我看看自己有没有被park");
+        }, "被park线程");
+
+        thread.start();
+        Thread.sleep(5000);
+        System.out.println(Thread.currentThread().getName());
+        LockSupport.unpark(thread);
+    }
+
+    public static void testInterrupt() {
+        Thread thread = new Thread(() -> {
+            for (int i = 0; i < 10000; ++i)
+                System.out.println("AAA");
+        }, "interrupt线程");
+        thread.start();
+        thread.interrupt();
+        System.out.println(thread.isInterrupted());
+        System.out.println(thread.isInterrupted());
+        System.out.println(thread.interrupted());
+        System.out.println(thread.interrupted());
+        System.out.println(thread.isAlive());
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        // testSleep();
+        //testJoin();
+        //testYield();
+        //testWaitAndNotify();
+        //testLockAndUnlock();
+        // testParkAndUnpark();
+        /*testInterrupt();
+        Thread.sleep(1000);
+        System.out.println("调试用的");*/
+
+
+        Thread.currentThread().interrupt();
+        System.out.println("第一次调用Thread.currentThread().interrupt()："
+                + Thread.currentThread().isInterrupted());
+        System.out.println("第一次调用thread.interrupted()："
+                + Thread.currentThread().interrupted());
+        System.out.println("第二次调用thread.interrupted()："
+                + Thread.currentThread().interrupted());
+    }
+
+}
+
---
+++ 结合ReentrantLock学习AQS.md
@@ -0,0 +1,238 @@
+AQS大致思想
+
+1. 有个原子变量 state， 记录锁的状态
+2. 有Node类型的head， tail
+3. Node内部类，有pre，next，thread， 大致就是能组成双向链表 大致就是想要阻塞的**线程**封装一下， 然后进入等待队里
+
+
+
+
+
+## Node
+
+```java
+//共享锁
+static final Node SHARED = new Node();
+//独占锁
+static final Node EXCLUSIVE = null;
+
+// 一下是waitStatus的值，  默认0
+ /** waitStatus value to indicate thread has cancelled */
+static final int CANCELLED =  1;
+// 线程需要被唤醒
+static final int SIGNAL    = -1;
+// 线程在等待某个条件
+static final int CONDITION = -2;
+        /**
+         * waitStatus value to indicate the next acquireShared should
+         * unconditionally propagate
+         */
+static final int PROPAGATE = -3;
+```
+
+
+
+
+
+hasQueuedPredecessors()
+
+```Java
+/**当前队列中有阻塞队列 ， 返回true
+当前线程是队列的队首或者 队列为为空， 返回false
+*/
+public final boolean hasQueuedPredecessors() {
+        // The correctness of this depends on head being initialized
+        // before tail and on head.next being accurate if the current
+        // thread is first in queue.
+        Node t = tail; // Read fields in reverse initialization order
+        Node h = head;
+        Node s;
+        return h != t &&
+            ((s = h.next) == null || s.thread != Thread.currentThread());
+}
+```
+
+
+
+## acquire(int arg)
+
+```Java
+public final void acquire(int arg) {
+        if (!tryAcquire(arg) &&   //获取锁不成功
+            acquireQueued(addWaiter(Node.EXCLUSIVE), arg)) // 并且成功加入队列
+            selfInterrupt();  //阻塞
+}
+```
+
+
+
+## tryAcquire(int acquires)
+
+```Java
+/** return: 获得锁-> true
+            未获得-> false
+*/
+protected final boolean tryAcquire(int acquires) {
+            final Thread current = Thread.currentThread();
+            int c = getState();
+            if (c == 0) {   //未上锁
+                if (!hasQueuedPredecessors() &&   //判断队列为空
+                    compareAndSetState(0, acquires)) {// cas更改锁的状态
+                    setExclusiveOwnerThread(current);// 标记当前线程为持有锁的线程
+                    return true;
+                }
+            }
+            else if (current == getExclusiveOwnerThread()) { // 可重入
+                int nextc = c + acquires;  //int 类型溢出
+                if (nextc < 0)
+                    throw new Error("Maximum lock count exceeded");
+                setState(nextc); //这里是自己持有锁，不用cas
+                return true;
+            }
+            return false;
+}
+```
+
+
+
+
+
+
+```Java
+// 返回锁的状态，  是不是自由状态
+public final boolean release(int arg) {
+        if (tryRelease(arg)) {  //锁自由了， 要进行唤醒操作
+            Node h = head;
+            if (h != null && h.waitStatus != 0)  // 队列不为空，并且 waitStatus是干嘛的???
+                unparkSuccessor(h);
+            return true;
+        }
+        return false;
+}
+```
+
+
+
+
+
+```Java
+protected final boolean tryRelease(int releases) {
+            int c = getState() - releases;
+            if (Thread.currentThread() != getExclusiveOwnerThread())
+                throw new IllegalMonitorStateException();
+            boolean free = false;       //标记可重入锁，是不是自由了（无锁状态）
+            if (c == 0) {
+                free = true;         // 无锁状态
+                setExclusiveOwnerThread(null); // 清除持有锁的线程
+            }
+            setState(c); 
+            return free;  //返回锁的状态
+}
+```
+
+
+
+
+
+
+
+```Java
+private Node addWaiter(Node mode) {
+        Node node = new Node(Thread.currentThread(), mode); 
+        // Try the fast path of enq; backup to full enq on failure
+        Node pred = tail; //pred: 之前
+        if (pred != null) {  //有队列
+            node.prev = pred;    //维护节点的关系， 这里为什么不放在下面更改呢
+            if (compareAndSetTail(pred, node)) {// cas,更改尾节点(其实已经改了tail的引用)
+                pred.next = node; // 维护
+                return node;
+            }
+        }
+        enq(node);
+        return node;
+}
+```
+
+```Java
+/*1. 队列为空， 先初始化一个 thread为null的Node节点（哨兵）。 并改变头尾节点， 再执行传进来的Node的入队操作。
+  2. 队列不为空， 直接入队 
+*/
+private Node enq(final Node node) {
+        for (;;) {  //cas 循环
+            Node t = tail;   //下面又进行了队列为空的判断，   并发情况???
+            if (t == null) { // Must initialize    队列为空，就出初始化队列
+                if (compareAndSetHead(new Node()))  // 设置头尾节点
+                    tail = head;
+            } else {                         // 和 addWaiter的部分操作一样
+                node.prev = t;
+                if (compareAndSetTail(t, node)) {
+                    t.next = node;
+                    return t;
+                }
+            }
+        }
+}
+```
+
+
+
+
+
+```Java
+final boolean acquireQueued(final Node node, int arg) { // 这里会有两次自旋去获取锁
+        boolean failed = true;  // failed: 标记 获得锁失败
+        try {
+            boolean interrupted = false;  // ??? 标记是否被打断???
+            for (;;) {  // 注意有循环
+                final Node p = node.predecessor();
+                if (p == head && tryAcquire(arg)) {   //如何node是队头（不含哨兵） ，并且当前线程获得锁成功
+                    setHead(node);   //重要！！！！这里把node的thread置为null
+                    p.next = null; // help GC
+                    failed = false;
+                    return interrupted;
+                }
+                if (shouldParkAfterFailedAcquire(p, node) &&
+                    parkAndCheckInterrupt())
+                    interrupted = true;
+            }
+        } finally {
+            if (failed)
+                cancelAcquire(node);
+        }
+}
+```
+
+
+
+
+
+```Java
+private static boolean shouldParkAfterFailedAcquire(Node pred, Node node) {
+        int ws = pred.waitStatus;   //waitStaus的作用???
+        if (ws == Node.SIGNAL)
+            /*
+             * This node has already set status asking a release
+             * to signal it, so it can safely park.
+             */
+            return true;
+        if (ws > 0) {
+            /*
+             * Predecessor was cancelled. Skip over predecessors and
+             * indicate retry.
+             */
+            do {
+                node.prev = pred = pred.prev;
+            } while (pred.waitStatus > 0);
+            pred.next = node;
+        } else {
+            /*
+             * waitStatus must be 0 or PROPAGATE.  Indicate that we
+             * need a signal, but don't park yet.  Caller will need to
+             * retry to make sure it cannot acquire before parking.
+             */
+            compareAndSetWaitStatus(pred, ws, Node.SIGNAL);
+        }
+        return false;
+}
+```
+
